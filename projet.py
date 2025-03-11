58c51fd721730d95e4108fb39c0deaa3ccee34cf
abdelhak saddi <abdelhaksaddi716@gmail.com>
2025-03-11 11:09:05 +0100

    last commit in main before pull on githi=ub

--- projet.py
+++ projet.py
@@ -1,12 +1,10 @@
 Data=[1,3,5]
 Som=sum(Data)
 print("La somme est:",Som)
-def somme(T):
- S=0
- for t in T:
-  S+=t
-return S
-Data=[1,3,5]
-Som=sum(Data)
-print("La somme est:",Som)
+if Data:
+ print("La somme est:",sum(Data))
+ print("Le min est:",min(Data))
+ print("Le max est:",max(Data))
+else:
+ print("Liste vide")
 
